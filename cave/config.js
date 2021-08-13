fa78244eadd9c5eff2102f64c4609a80ca37052a
thanhcong12122000 <72177135+thanhcong12122000@users.noreply.github.com>
2021-08-13 21:45:38 +0700

    Add files via upload

---
+++ cave/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Huy Điếm',
+    introDesc: `Ditconmemay`,
+    btnIntro: 'Ấn vào đây ngay',
+    title: 'Hoàng Quang Huy, Bú Cu Theo Cách Của Bạn',
+    desc: 'Bạn sẽ làm gì nếu bạn là Hoàng Quang Huy và đang bị tên cướp rượt đuổi?? ',
+    btnYes: 'Bú cu nó',
+    btnNo: 'Chạy',
+    question:'Ai là con đĩ của anh?',
+    btnReply: 'Gửi cho anh',
+    reply: 'Dạ Huy',
+    mess: 'Anh biết mà. Con đĩ bún đậu ăn mắm tôm',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100026010852096' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
